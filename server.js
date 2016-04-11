b3e44442dcdc1b50643c65c1d598a05b1a07e2b4
Hellon Canella <helloncanella@gmail.com>
2016-04-11 04:25:10 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+var http = require('http');
+var path = require('path');
+var request = require('request');
+
+var express = require('express');
+
+var router = express();
+var server = http.createServer(router);
+
+router.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
+router.use('/', function(req, res) {
+  var url = 'http://www.band.uol.com.br/rss/colunista_266.xml';
+  req.pipe(request(url)).pipe(res);  
+});
+
+
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+  var addr = server.address();
+});
