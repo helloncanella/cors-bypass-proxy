41a4ea84fed1906fe7ac2058f92cdcf5e3235618
Hellon Canella <helloncanella@gmail.com>
2016-09-11 21:33:36 +0000

    ok

--- server.js
+++ server.js
@@ -12,13 +12,27 @@ var url;
 router.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Content-Type", "application/json; charset=utf-8");
   next();
 });
 
 
 router.get('/', function(req, res) {
   var url = req.query.url ||'http://www.band.uol.com.br/rss/colunista_266.xml';
-  req.pipe(request(url)).pipe(res);
+  
+  // req('http://www.band.uol.com.br/rss/colunista_266.xml')
+  
+  request('http://www.band.uol.com.br/rss/colunista_266.xml', function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      body = body.replace(/ISO-8859-1/,'UTF-8')
+      return unescape(encodeURIComponent(body)) // Show the HTML for the Google homepage. 
+    }
+  }).pipe(res)
+  
+  // req.pipe(request(url)).pipe(res);
+ 
+  
+ 
 });
 
 
